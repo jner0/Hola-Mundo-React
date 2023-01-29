d84a7d67413d248f7dd6f1163bff1472ee466587
Nemecio Rodriguez <jnemecio10@gmail.com>
2023-01-29 15:31:41 -0500

    Aprendiendo Proptypes en React

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,19 +1,32 @@
 //import { Fragment } from "react";
+import PropTypes from 'prop-types';
+
 const getResult = (a,b) =>{
   return a + b;
 };
 
-export const FirstApp = () => {
-
-
+export const FirstApp = ( {title, subTitle, name} ) => {
+  
   return (       
     //<Fragment>
     <>  
-      <h1>{ getResult(1,11) }</h1>
+      <h1>{ title }</h1>
       {/* <code> { JSON.stringify(newMessage) } </code> */}
-      <p>Soy un subtitulo</p>
+      <p>{subTitle }</p>
+      <p>{name}</p>
     </>  
     //</Fragment>
 
   );
+}
+
+FirstApp.propTypes = {
+  title: PropTypes.string.isRequired,
+  subTitle: PropTypes.string.isRequired
+}
+
+FirstApp.defaultProps = {
+  title: 'No hay nungun Titulo',
+  subTitle: 'No Hay Subtitulo',
+  name: 'Nemecio Rodriguez',
 }
