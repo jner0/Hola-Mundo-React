bab67cc6992fa8a00b81304891605a1f23e76b49
Nemecio Rodriguez <jnemecio10@gmail.com>
2023-01-29 21:13:41 -0500

    Hooks y Events

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -9,16 +9,24 @@ export const CounterApp = ( {value} ) => {
     const handleAdd = () => {
         //setCounter( counter + 1);
         setCounter( (c) => c + 1 )
-    } 
+    }; 
+
+    const handleSubstract = () => {
+        setCounter( counter - 1);
+    };
+
+    const handleReset = () => {
+        setCounter (value);
+    };
 
     return(
       <>
         <h1> Counter App </h1>
         <h2> { counter } </h2>
 
-        <button onClick={ handleAdd }>
-          +1
-        </button>
+        <button onClick={ handleAdd }> +1 </button>
+        <button onClick={ handleSubstract }> -1 </button>
+        <button onClick={ handleReset }> Reset </button>
       </>
     );
 }
